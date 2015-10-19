c5687a3f0db9c4f3db1d87186735e2517a03201b
Web-App-Dev <loweman64@outlook.com>
2015-10-19 11:54:38 -0400

    Added the feature to remove files/folders that are older than a certain age

--- Backup scripts/backup final.py
+++ Backup scripts/backup final.py
@@ -1,38 +1,54 @@
-from subprocess import call 
-import os, datetime
+from subprocess import call
+import os, datetime, time, sys
 
-# Variables #################################
-Destination = "/Users/alowe/Desktop/testdir"
-Source = "/debian/project/trace/"
-Site = "ftp.debian.org"
-
+# Options #################################
 Username = " "
 Password = " "
+Site = "ftp.debian.org"
+Source = "/path/to/source"
+Destination = "/path/to/destination"
 #############################################
 
-if os.path.exists(Destination) == False:
-	os.mkdir(Destination)
+linux = sys.platform.startswith('linux')
+darmin = sys.platform.startswith('darmin')
+osx2 = sys.platform.startswith('os2')
+os2emax = sys.platform.startswith('os2emax')
 
-now = datetime.datetime.now()
-Year = str(now.day) + "-" + str(now.month) + "-" + str(now.year)
-Time = str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
-directoryName = Year + " " + Time
+if linux == linux or darmin == darmin or osx2 == osx2 or os2emax == os2emax:
 
-Url = Site + Source
+	if os.path.exists(Destination) == False:
+		os.mkdir(Destination)
 
-if Username and Password == " ":
-	Url = "ftp://" + Url
+	nowDatetime = datetime.datetime.now()
+	Year = str(nowDatetime.month) + "-" + str(nowDatetime.day) + "-" + str(nowDatetime.year)
+	Time = str(nowDatetime.hour) + ":" + str(nowDatetime.minute) + ":" + str(nowDatetime.second)
+	directoryName = Year + " " + Time
+
+	Url = Site + Source
+
+	if Username and Password == " ":
+		Url = "ftp://" + Url
+	else:
+		Url = "ftp://" + Username + ":" + Password + Url
+
+	os.mkdir(Destination + "/" + directoryName + "/")
+	os.chdir(Destination + "/" + directoryName)
+
+	def backUp(Source):
+		call(["wget", "-r", Source])
+
+	backUp(Url)
+
+	os.chdir(Destination)
+	call(["tar", "-jcvf", directoryName + ".tar.bz2", "-P", Destination + "/" + directoryName])
+	call(["rm", "-rf", directoryName])
+
+	nowTime = time.time()
+
+	for file in os.listdir(Destination):
+		file = os.path.join(Destination, file)
+		if os.stat(os.path.join(Destination, file)).st_mtime <= nowTime - 30 * 86400:
+			if os.path.isfile(file):
+				os.remove(os.path.join(Destination, file))
 else:
-	Url = "ftp://" + Username + ":" + Password + Url
-
-os.mkdir(Destination + "/" + directoryName + "/")
-os.chdir(Destination + "/" + directoryName)
-
-def backUp(Source):
-	call(["wget", "-r", Source])
-
-backUp(Url)
-
-os.chdir(Destination)
-call(["tar", "-jcvf", directoryName + ".tar.bz2", "-P", Destination + "/" + directoryName])
-call(["rm", "-rf", directoryName])
+	print "This script is not supported on", sys.platform
